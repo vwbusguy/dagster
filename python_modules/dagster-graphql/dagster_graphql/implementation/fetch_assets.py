2ea803dccd0e1d172b44a3517da47463460e7a47
Sean Mackesey <s.mackesey@gmail.com>
2022-12-11 10:59:07 -0500

    [gql] AssetKey cleanup

--- python_modules/dagster-graphql/dagster_graphql/implementation/fetch_assets.py
+++ python_modules/dagster-graphql/dagster_graphql/implementation/fetch_assets.py
@@ -1,6 +1,6 @@
 import datetime
 from collections import defaultdict
-from typing import TYPE_CHECKING, Dict, Iterator, List, Mapping, Optional, Sequence, Tuple
+from typing import TYPE_CHECKING, AbstractSet, Dict, Iterator, List, Mapping, Optional, Sequence, Tuple
 
 from dagster_graphql.implementation.loader import (
     CrossRepoAssetDependedByLoader,
@@ -90,7 +90,7 @@ def asset_node_iter(
                 yield location, repository, external_asset_node
 
 
-def get_asset_node_definition_collisions(graphene_info: HasContext, asset_keys: Sequence[AssetKey]):
+def get_asset_node_definition_collisions(graphene_info: HasContext, asset_keys: AbstractSet[AssetKey]):
     from ..schema.asset_graph import GrapheneAssetNodeDefinitionCollision
     from ..schema.external import GrapheneRepository
 
