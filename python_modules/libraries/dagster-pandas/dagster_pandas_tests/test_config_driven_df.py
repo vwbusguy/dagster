f21f1e39b4a07040ff446c52220e9c258e8197b1
Sean Mackesey <s.mackesey@gmail.com>
2022-12-12 13:18:41 -0500

    [dagster-pandas] misc

--- python_modules/libraries/dagster-pandas/dagster_pandas_tests/test_config_driven_df.py
+++ python_modules/libraries/dagster-pandas/dagster_pandas_tests/test_config_driven_df.py
@@ -11,14 +11,14 @@ from dagster._utils.test import get_temp_file_name
 
 def check_parquet_support():
     try:
-        import pyarrow  # noqa: F401
+        import pyarrow  # pyright: ignore  # noqa: F401
 
         return
     except ImportError:
         pass
 
     try:
-        import fastparquet  # noqa: F401
+        import fastparquet  # pyright: ignore  # noqa: F401
 
         return
     except ImportError:
