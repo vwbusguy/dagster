acb2fa1c604dda9d8e213bcf7113395045e7e8d8
Sean Mackesey <s.mackesey@gmail.com>
2022-12-18 17:59:36 -0500

    bump packaging dep to v22

--- python_modules/libraries/dagstermill/dagstermill/compat.py
+++ python_modules/libraries/dagstermill/dagstermill/compat.py
@@ -5,8 +5,6 @@ from papermill.exceptions import PapermillExecutionError
 
 def is_papermill_2():
     version = parse(papermill.__version__)
-    # satisfies typechecker that might think version is a LegacyVersion
-    assert isinstance(version, Version)
     return version.major == 2
 
 
